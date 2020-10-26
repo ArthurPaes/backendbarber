1263c3744a3de805b6c85f9cbefd01eeb854cfe5
arthur <arthur_pls_@hotmail.com>
2020-10-26 01:49:35 -0300

    committ

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,9 +2,8 @@ import { container } from 'tsyringe';
 import mailConfig from '@config/mail';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
-import SESMailProvider from './MailProvider/implementations/SESMailProvider';
 import IMailProvider from './MailProvider/models/IMailProvider';
-import EtherealMailProvider from './MailProvider/implementations/EtherealMailProvider';
+import mailProviders from './MailProvider';
 
 import IMailTemplateProvider from './MailTemplateProvider/models/IMailTemplateProvider';
 import HandlebarsMailTemplateProvider from './MailTemplateProvider/implementations/HandlebarsMailTemplateProvider';
@@ -21,7 +20,5 @@ container.registerSingleton<IMailTemplateProvider>(
 
 container.registerInstance<IMailProvider>(
   'MailProvider',
-  mailConfig.driver === 'ethereal'
-    ? container.resolve(EtherealMailProvider)
-    : container.resolve(SESMailProvider),
+  mailProviders[mailConfig.driver],
 );
